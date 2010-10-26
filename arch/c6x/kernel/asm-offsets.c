be37b0d6a2e972315f4d82bd8c10794aa7098de9
Aurelien JACQUIOT-XID <x0134876@lnx0134876.tif.ti.com>
2010-10-26 10:41:31 +0200

    Add missing THREAD_INFO_PREEMPT_COUNT computation
    Signed-off-by: Aurelien JACQUIOT-XID <x0134876@lnx0134876.tif.ti.com>

--- arch/c6x/kernel/asm-offsets.c
+++ arch/c6x/kernel/asm-offsets.c
@@ -84,7 +84,8 @@ void foo(void)
 	OFFSET(TASK_STATE,	task_struct, state);
 	BLANK();
 
-	OFFSET(THREAD_INFO_FLAGS,	thread_info, flags);
+	OFFSET(THREAD_INFO_FLAGS,	  thread_info, flags);
+	OFFSET(THREAD_INFO_PREEMPT_COUNT, thread_info, preempt_count);
 	BLANK();
 
 	/* These would be unneccessary if we ran asm files
