a5de2002a87f4b666decd6aafec5445aee74c5da
Aurelien JACQUIOT-XID <x0134876@lnx0134876.tif.ti.com>
2010-11-18 14:00:13 +0100

    Add a C6x implementation of ndelay() to fix small delays (like NAND Flash driver)
       - Implement C6x specific ndelay() with a real nanosec granularity based on calibrated loop
       - Remove division
       - Only use multiplication for variable based delays
       - Test too small delay
       - Also improve udelay() in the same manner
    
    Signed-off-by: Aurelien JACQUIOT-XID <x0134876@lnx0134876.tif.ti.com>

--- arch/c6x/include/asm/delay.h
+++ arch/c6x/include/asm/delay.h
@@ -23,6 +23,40 @@ static inline void __delay(unsigned long loop)
 	_c6x_delay(loop / 3); /* Because a loop takes 6 cycles on C6x instead
 				 of 2 on Intel and others */
 }
+
+/*
+ * Nanosecond delay for variable. Do not use division, only one multiplication 
+ */
+#define NSEC_FACTOR_SHIFT  6
+#define NSEC_FACTOR_MULT   ((CONFIG_TMS320C6X_MHZ << NSEC_FACTOR_SHIFT) / (1000 * 6))
+#define NSEC_CEIL          ((1 << NSEC_FACTOR_SHIFT) / NSEC_FACTOR_MULT)
+
+static inline void __ndelay(unsigned long nsec)
+{
+	if (nsec > NSEC_CEIL)
+		_c6x_delay((nsec * NSEC_FACTOR_MULT) >> NSEC_FACTOR_SHIFT);
+}
+
+
+/*
+ * Nanosecond delay for small delays. Of course, it needs to be used with constant for small values.
+ */
+#define ndelay(n)            (__builtin_constant_p(n) ?			\
+			      (n) <= NSEC_CEIL ? :			\
+			      _c6x_delay(((n) * CONFIG_TMS320C6X_MHZ) / (1000 * 6)) : \
+	                      __ndelay(n))
+
+/*
+ * Microsecond delay for variable. Do not use division, only one multiplication 
+ */
+#define USEC_FACTOR_SHIFT  2
+#define USEC_FACTOR_MULT   ((CONFIG_TMS320C6X_MHZ << USEC_FACTOR_SHIFT) / 6)
+
+static inline void __udelay(unsigned long usec)
+{
+	_c6x_delay((usec * USEC_FACTOR_MULT) >> USEC_FACTOR_SHIFT);
+}
+
 /*
  * Use only for very small delays ( < 1 msec).  Should probably use a
  * lookup table, really, as the multiplications take much too long with
@@ -30,12 +64,8 @@ static inline void __delay(unsigned long loop)
  * first constant multiplications gets optimized away if the delay is
  * a constant)  
  */
-static inline void udelay(unsigned long usecs)
-{
-	/* A loop takes 6 cycles on C6x */
-	_c6x_delay((usecs * CONFIG_TMS320C6X_MHZ) / 6);
-}
-
-#define muldiv(a, b, c)    (((a)*(b))/(c))
+#define udelay(n)            (__builtin_constant_p(n) ?			\
+			      _c6x_delay(((n) * CONFIG_TMS320C6X_MHZ) / 6) : \
+			      __udelay(n))
 
 #endif /* __ASM_C6X_DELAY_H */
