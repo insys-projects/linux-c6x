f026794a82ad169624851aef82a644743c71c0e2
Aurelien JACQUIOT-XID <x0134876@lnx0134876.tif.ti.com>
2011-04-15 13:37:06 +0200

    Use 4-bit hardware ECC for the EMIF NAND Flash of the EVM6678L
    Signed-off-by: Aurelien JACQUIOT-XID <x0134876@lnx0134876.tif.ti.com>

--- arch/c6x/platforms/board-evm6678.c
+++ arch/c6x/platforms/board-evm6678.c
@@ -312,7 +312,7 @@ static struct davinci_nand_pdata evmc6678_nand_data = {
 	.parts			= evm6678_nand_parts,
 	.nr_parts		= ARRAY_SIZE(evm6678_nand_parts),
 	.ecc_mode		= NAND_ECC_HW,
-	.ecc_bits               = 1,
+	.ecc_bits               = 4,
 	.options	        = NAND_USE_FLASH_BBT,
 };
 
