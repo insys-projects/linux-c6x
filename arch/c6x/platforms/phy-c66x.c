6b2ea6966872e52ea346bfffbaff9322d402c395
Aurelien Jacquiot <a-jacquiot@ti.com>
2011-12-12 14:30:39 +0100

    C66x: Fixed Ethernet PHY init to use the KeyStone SGMII generic driver
    Signed-off-by: Aurelien Jacquiot <a-jacquiot@ti.com>

--- arch/c6x/platforms/phy-c66x.c
+++ arch/c6x/platforms/phy-c66x.c
@@ -16,18 +16,20 @@
 #include <linux/kernel.h>
 #include <linux/delay.h>
 #include <linux/types.h>
+#include <linux/io.h>
+#include <linux/err.h>
 
 #include <asm/setup.h>
 #include <asm/machdep.h>
 #include <asm/io.h>
-#include <asm/sgmii.h>
 #include <asm/dscr.h>
 
 #include <mach/keystone_qmss.h>
 #include <mach/keystone_netcp.h>
 #include <mach/keystone_pa.h>
-
 #include <mach/keystone_cpsw.h>
+
+#include <linux/keystone/sgmii.h>
 
 static int serdes_init(void)
 {
@@ -58,7 +60,7 @@ static int serdes_init(void)
 
 static int sgmii_init(void)
 {
-	struct sgmii_config_s sgmiic0, sgmiic1;
+	struct sgmii_config sgmiic0, sgmiic1;
 
 	sgmiic0.master    = 1;
 	sgmiic0.loopback  = 0;
@@ -75,7 +77,7 @@ static int sgmii_init(void)
 #else
 	sgmiic0.auxconfig = 0x00000041; /* PLL multiplier */
 #endif
-	sgmii_config(0, &sgmiic0);
+	keystone_sgmii_config(0, &sgmiic0);
 
 	sgmiic1.master    = 1;
 	sgmiic1.loopback  = 0;
@@ -92,14 +94,14 @@ static int sgmii_init(void)
 #else
 	sgmiic1.auxconfig = 0x00000041; /* PLL multiplier */
 #endif
-	sgmii_config(1, &sgmiic1);
+	keystone_sgmii_config(1, &sgmiic1);
 
 	printk("SGMII init complete\n");
 
 	return 0;
 }
 
-static int hw_cpsw_config(u32 ctl, u32 max_pkt_size)
+static int cpsw_config(u32 ctl, u32 max_pkt_size)
 {
 	u32 i;
 
@@ -121,7 +123,7 @@ static int hw_cpsw_config(u32 ctl, u32 max_pkt_size)
 	for (i = 0; i < CPSW_NUM_PORTS; i++)
 		__raw_writel(CPSW_REG_VAL_PORTCTL_FORWARD_MODE,
 			     (DEVICE_CPSW_BASE + CPSW_REG_ALE_PORTCTL(i)));
-	
+
 	return 0;
 }
 
@@ -129,9 +131,12 @@ int evm_phy_init(void)
 {	
 	if (get_coreid() == get_master_coreid()) {
 
+		/* Initialize SGMII driver */
+		keystone_sgmii_init();
+
 		/* Reset SGMII */
-		sgmii_reset(0);
-		sgmii_reset(1);
+		keystone_sgmii_reset(0);
+		keystone_sgmii_reset(1);
 
 		/* SERDES init */
 		serdes_init();
@@ -140,7 +145,7 @@ int evm_phy_init(void)
 		sgmii_init();
 		
 		/* Enable port 0 with max pkt size to 9000 */
-		hw_cpsw_config(CPSW_CTL_P0_ENABLE, 9000);
+		cpsw_config(CPSW_CTL_P0_ENABLE, 9000);
 	}
 
 	return 0;
