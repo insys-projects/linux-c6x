56734b4f2825d56232d4cc76160084ec76cea595
Aurelien Jacquiot <a-jacquiot@ti.com>
2012-10-09 10:26:13 +0200

    C6x: Override _STK_LIM value for more realistic MMU-less stack size
     _STK_LIM is used by NPTL thread library for defining the thread stack size.
     As on MMU-less stack is really allocated, we need to override this default
     _STK_LIM value (8MB) to a more convenient size (128KB).
    
    Signed-off-by: Aurelien Jacquiot <a-jacquiot@ti.com>

--- arch/c6x/include/asm/resource.h
+++ arch/c6x/include/asm/resource.h
@@ -13,6 +13,17 @@
 #ifndef __ASM_C6X_RESOURCE_H
 #define __ASM_C6X_RESOURCE_H
 
+#ifdef _STK_LIM
+#undef _STK_LIM
+#endif
+/*
+ * We do not have MMU meaning that stack is really allocated and wastes memory.
+ * For example NPTL relies on this value for allocating thread stacks.
+ * Thus align this value on the stack size generated in the ELF C6x
+ * binaries (128KB).
+ */
+#define _STK_LIM  (128 * 1024)
+
 #include <asm-generic/resource.h>
 
 #endif /* __ASM_C6X_RESOURCE_H */
